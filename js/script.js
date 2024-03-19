28356de4c4c9d3ff9eb794d3a6d076b1ef8468a4
Fethawit <159185142+fetuha-gebremekeal@users.noreply.github.com>
2024-03-19 18:39:50 +0000

    d

--- js/script.js
+++ js/script.js
@@ -13,5 +13,5 @@ function enterClicked() {
   const streetNumber = parseInt(document.getElementById("street-number").value)
 
   // output
-  document.getElementById("user-adress").innerHTML = "Your info is: " + streetName + ", streetNumber " + streetNumber + "."
+  document.getElementById("user-address").innerHTML = "Your info is: " + streetName + ", streetNumber " + streetNumber + "."
 }
