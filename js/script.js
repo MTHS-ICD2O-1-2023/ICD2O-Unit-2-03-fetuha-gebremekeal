3c2c27a96acb2c33a91fbec64095965281cb963b
Fethawit <159185142+fetuha-gebremekeal@users.noreply.github.com>
2024-03-19 18:13:55 +0000

    streetname

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Fetuha All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Fetuha
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+
+/**
+ * This function gets users street name and street number and shows it back to user.
+ */
+function enterClicked() {
+  // input
+  const streetName = document.getElementById("street-name").value
+  const StreetNumber = parseInt(document.getElementById("street-number").value)
+
+  // output
+  document.getElementById("user-info").innerHTML =
+    "Your info is: " + streetName + ", streetNumber " + streetNumber + "."
+}
