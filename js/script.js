497c6a47ea69a5850e6f989d08b1bef0f4e0c93a
Fethawit <159185142+fetuha-gebremekeal@users.noreply.github.com>
2024-03-19 18:26:50 +0000

    favicon

--- js/script.js
+++ js/script.js
@@ -13,6 +13,5 @@ function enterClicked() {
   const StreetNumber = parseInt(document.getElementById("street-number").value)
 
   // output
-  document.getElementById("user-adress").innerHTML =
-    "Your info is: " + streetName + ", streetNumber " + streetNumber + "."
+  document.getElementById("user-adress").innerHTML = "Your info is: " + streetName + ", streetNumber " + streetNumber + "."
 }
